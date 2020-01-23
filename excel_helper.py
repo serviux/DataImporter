579ecdd68d9fd3907547d2429c9089742265db86
jcorm714 <jcorm714@gmail.com>
2020-01-23 01:50:43 -0500

    Added comments minor refactoring

--- excel_helper.py
+++ excel_helper.py
@@ -1,16 +1,30 @@
 import openpyxl
-import json
 import player
 
 class ExcelInterface:
-    def __init__(self, path_to_workbook = ""):
+    """
+    This class is used to interface with the excel document cointaining all the players
+    matches and win loss ratios.
+
+    args: path_to_workbook --path to the file being taken in.
+    """
+    def __init__(self, path_to_workbook):
         if len(path_to_workbook) == 0:
             raise Exception("No workbook loaded")
         self.wb = openpyxl.load_workbook(path_to_workbook)
     
+    #stores the sheets in the current instance
     def load_sheets(self):
         self.sheets = self.wb.sheetnames
 
+    """
+    gets a list of all players from all tabs in the excel doc
+    and adds them to a dictionary. Names are forced to be uppercase 
+    so they are case Insensitive in the word doc
+
+    creates the self.entries dictionary a dictionary 
+    containing a key value pair like <String, Player>
+    """
     def get_entries(self): 
         
         self.entries = dict()
@@ -30,10 +44,15 @@ class ExcelInterface:
                 elif str.upper(val.value) not in self.entries:
                     self.entries[str.upper(val.value)] =  player.Player(name = str.upper(val.value))
     
+
+    """
+    used to create a list of all the players converted to a 
+    json object. 
+    """
     def playersToJSON(self):
         players = []
         for key in self.entries.keys():
-            players.append(json.dumps(self.entries[key].__dict__))
+            players.append(player.PlayerToJSON.PlayerToJSON(self.entries[key]))
         return players
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,13 +1,45 @@
 import json
 
 class Player:
-    
+    """
+    This class keeps track of players in smash league
+    Args:
+        name: username that player has chosen. Has to be unique, main identification for a player.
+        wins: number of matches that a player has one
+        losses: number of matches a player has lost
+        last_five: list of the names that the other players. Only stores up to five names
+    """
     def __init__(self, name = "", wins = 0, losses = 0,  last_five = []):
         self.name = name
         self.wins = wins
         self.losses = losses
         self.last_five = last_five
     
+"""
+Helper class to the player class. 
+Converts a player object a json object 
+
+example 
+    p = Player(name = "Jeff" , wins = 15, losses= 7)
+    p.last_five= ["Jeff1","Jeff2","Jeff3","Jeff4","Jeff5"]
+    print(PlayerToJSON.PlayerToJSON(p))
+
+expected ouput
+{
+  "name": "Jeff",
+  "wins": 15,
+  "losses": 7,
+  "last_five": [
+    "Jeff1",
+    "Jeff2",
+    "Jeff3",
+    "Jeff4",
+    "Jeff5"
+  ]
+}
+
+"""
+
 class PlayerToJSON:
 
     @staticmethod
